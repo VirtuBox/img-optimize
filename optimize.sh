0916c32b4c2f3890815afba56ecb570fce1667d9
VirtuBox <contact@virtubox.net>
2016-10-16 18:31:18 +0200

    Create optimize.sh
    Optimize.sh creation

---
+++ optimize.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+echo ""
+echo "Welcome to wp-optimize script"
+echo ""
+apt-get update &>/dev/null
+apt-get install optipng jpegoptim -y &>/dev/null
+cd /var/www
+find . -name *.jp* | xargs jpegoptim --strip-all -m76
+find . -iname '*.png' -print0 | xargs -0 optipng -o7 -preserve
+echo ""
+echo "Optimization Process done :)"
+echo ""
