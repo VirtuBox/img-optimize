0ce712acb1ee706e0d6ddf29debdfdbc2d5dd660
VirtuBox <contact@virtubox.net>
2018-08-09 21:45:12 +0200

    fix variable

--- optimize.sh
+++ optimize.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CSI="\\033["
+CSI='\033['
 CEND="${CSI}0m"
 CGREEN="${CSI}1;32m"
 
@@ -12,67 +12,64 @@ echo "Welcome to optimize.sh image optimization script."
 echo ""
 
 echo ""
-echo "Do you want to optimize all jpg images in "$1" ? (y/n)"
+echo "Do you want to optimize all jpg images in $1 ? (y/n)"
 while [[ $jpg != "y" && $jpg != "n" ]]; do
     read -p "Select an option [y/n]: " jpg
 done
 echo ""
-echo "Do you want to optimize all png images in "$1" (it may take a while) ? (y/n)"
+echo "Do you want to optimize all png images in $1 (it may take a while) ? (y/n)"
 while [[ $png != "y" && $png != "n" ]]; do
     read -p "Select an option [y/n]: " png
 done
 echo ""
-echo "Do you want to convert all jpg & png images to WebP in "$1" ? (y/n)"
+echo "Do you want to convert all jpg & png images to WebP in $1 ? (y/n)"
 while [[ $webp != "y" && $webp != "n" ]]; do
     read -p "Select an option [y/n]: " webp
 done
 
 # optimize jpg
 jpg_optimize() {
-find "$1" -type f \( -iname "*.jpg" -o -iname "*.jpeg" \) -print0 | xargs -0 jpegoptim --preserve --quiet --strip-all -m82 
+    find "$1" -type f \( -iname "*.jpg" -o -iname "*.jpeg" \) -print0 | xargs -0 jpegoptim --preserve --quiet --strip-all -m82
 
-        echo -ne "       jpg optimization                      [${CGREEN}OK${CEND}]\\r"
-        echo -ne "\\n"
+    echo -ne "       jpg optimization                      [${CGREEN}OK${CEND}]\\r"
+    echo -ne '\n'
 }
 # optimize png
 png_optimize() {
-find "$1" -type f  -iname '*.png' -print0 | xargs -0 optipng -o7 -strip all -quiet 
+    find "$1" -type f -iname '*.png' -print0 | xargs -0 optipng -o7 -strip all -quiet
 
-        echo -ne "       png optimization                      [${CGREEN}OK${CEND}]\\r"
-        echo -ne "\\n"
+    echo -ne "       png optimization                      [${CGREEN}OK${CEND}]\\r"
+    echo -ne '\n'
 }
 # convert png to webp
 webp_convert_images() {
-find "$1" -type f -iname "*.png" -print0 | xargs -0 -I {}  \
-bash -c 'webp_version="$0".webp
-if [ ! -f "$webp_version" ]; then
-{ cwebp -quiet -z 9 -mt {} -o {}.webp; }
-fi' 
+    find "$1" -type f -iname "*.png" -print0 | xargs -0 -I {} \
+        bash -c 'webp_version="$0".webp
+    if [ ! -f "$webp_version" ]; then
+    { cwebp -quiet -z 9 -mt {} -o {}.webp; }
+    fi'
 
-        echo -ne "       png to webp conversion                      [${CGREEN}OK${CEND}]\\r"
-        echo -ne "\\n"
+    echo -ne "       png to webp conversion                      [${CGREEN}OK${CEND}]\\r"
+    echo -ne '\n'
 
-# convert jpg to webp
-find "$1" -type f \( -iname "*.jpg" -o -iname "*.jpeg" \) -print0 | xargs -0 -I {} \
-bash -c 'webp_version="$0".webp
+    # convert jpg to webp
+    find "$1" -type f \( -iname "*.jpg" -o -iname "*.jpeg" \) -print0 | xargs -0 -I {} \
+        bash -c 'webp_version="$0".webp
 if [ ! -f "$webp_version" ]; then
 { cwebp -quiet -q 82 -mt {} -o {}.webp; }
 fi'
 
-        echo -ne "       jpg to webp conversion                      [${CGREEN}OK${CEND}]\\r"
-        echo -ne "\\n"
+    echo -ne "       jpg to webp conversion                      [${CGREEN}OK${CEND}]\\r"
+    echo -ne '\n'
 }
 
-if [ "$jpg" = "y" ]
-then
+if [ "$jpg" = "y" ]; then
     jpg_optimize "$@"
 fi
-if [ "$png" = "y" ]
-then
+if [ "$png" = "y" ]; then
     png_optimize "$@"
 fi
-if [ "$webp" = "y" ]
-then
+if [ "$webp" = "y" ]; then
     webp_convert_images "$@"
 fi
 
