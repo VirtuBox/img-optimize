13bf8d77524f865e64edd6bd19351e3a3ee69b6e
VirtuBox <thomas@virtubox.net>
2019-07-12 14:29:29 +0200

    Update webp install script & compile libwebp

--- install-webp.sh
+++ install-webp.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 # install prerequisites
-sudo apt-get install build-essential libjpeg-dev libpng-dev libtiff-dev libgif-dev libwebp-dev -y
+apt-get install build-essential libjpeg-dev libpng-dev libtiff-dev libgif-dev libwebp-dev tar gzip wget -y
 
 # go into /usr/local/src and remove previous libwebp folder/archive
 cd /usr/local/src || exit
 rm -rf libwebp*
 
 # download and extract latest libwebp sources
-wget https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.0.0.tar.gz -O libwebp.tar.gz
+wget https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.0.2.tar.gz -O libwebp.tar.gz
 tar xvzf libwebp.tar.gz
 cd libwebp-* || exit
 
 # configure libwebp and launch compilation
-./configure
+./configure --prefix=/usr
 make -j "$(nproc)"
-sudo make install
+make install
+strip --strip-unneeded /usr/bin/cwebp
