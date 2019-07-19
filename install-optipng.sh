0607ede078ae9176b53bec518fc123e186fd9448
VirtuBox <thomas@virtubox.net>
2019-07-19 10:56:12 +0200

    update travis for testing

--- install-optipng.sh
+++ install-optipng.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 # install prerequisites
-sudo apt-get install build-essential libpng-dev -y
+apt-get install build-essential libpng-dev zlib1g-dev curl pigz -y
 
 # go into /usr/local/src and remove previous optipng folder/archive
 cd /usr/local/src || exit 1
 rm -rf optipng*
 
 # get the latest optipng release link
-OPTIPNGLATEST=$(wget http://optipng.sourceforge.net/ -O - | grep tar.gz | awk -F '["]' '{print $4}')
+OPTIPNGLATEST=$(curl -sL optipng.sourceforge.net | grep tar.gz | awk -F '["]' '{print $4}')
 
 # download and extract optipng
 wget $OPTIPNGLATEST -O optipng.tar.gz
-tar -xf optipng.tar.gz
+tar -I pigz -xf optipng.tar.gz
 cd optipng-* || exit 1
 
 # configure and compile optipng
 ./configure --prefix=/usr
 make -j "$(nproc)"
-sudo make install
+strip --strip-unneeded /usr/local/src/optipng-*/src/optipng/optipng
+make install
